de206e5691f13f013e2fd5d4070880763ecbb4ed
Lucas Correia Dupuy <lucorrei@mi-r3-p4.s19.be>
2025-04-24 16:34:11 +0200

    added strjoin variants

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,16 +6,18 @@
 /*   By: lucorrei <lucorrei@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 18:08:22 by lucorrei          #+#    #+#             */
-/*   Updated: 2025/04/14 18:36:07 by lucorrei         ###   ########.fr       */
+/*   Updated: 2025/04/24 16:02:11 by lucorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*res;
 	int		total;
 
+	if (!s1 || !s2)
+		return (NULL);
 	total = ft_strlen(s1) + ft_strlen(s2) + 1;
 	res = 0;
 	res = ft_calloc(total, 1);
@@ -23,5 +25,33 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	ft_strlcpy(res, s1, total);
 	ft_strlcat(res, s2, total);
+	return (res);
+}
+
+char	*ft_strjoin_free_second(char *s1, char *s2)
+{
+	char	*res;
+
+	res = ft_strjoin(s1, s2);
+	free(s2);
+	return (res);
+}
+
+char	*ft_strjoin_free_first(char *s1, char *s2)
+{
+	char	*res;
+
+	res = ft_strjoin(s1, s2);
+	free(s1);
+	return (res);
+}
+
+char	*ft_strjoin_free_both(char *s1, char *s2)
+{
+	char	*res;
+
+	res = ft_strjoin(s1, s2);
+	free(s1);
+	free(s2);
 	return (res);
 }
